e7e3a4f182ccd7efcd72e5f6a5efec72a8e31b75
Roger Kirkness <roger@convictional.com>
2018-03-18 17:16:11 -0400

    Bump version to 1.0.4

--- sw.js
+++ sw.js
@@ -1,15 +1,42 @@
+---
+layout: null
+---
 
+var urls = [
+  {% for post in site.posts %}
+  '{{ post.url }}',
+  {% endfor %}
+  '/'
+]
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting()
   event.waitUntil(
-    caches.open('rogerkirkness').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/writing',
-        '/following',
-        '/paperless',
-        '/on-leadership'
-      ])
+    caches.open('rogerkirkness-1').then((cache) => {
+      return cache.addAll(urls)
     })
   )
+})
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.filter(function(cacheName){
+          return cacheName.startsWith("rogerkirkness")
+            && cacheName != staticCacheName
+        }).map(function(cacheName){
+          return cache.delete(cacheName)
+        })
+      )ß
+    })
+  )
+})
+
+self.addEventListener("fetch", function(e){
+  e.respondWith(
+     caches.match(e.request).then(function(response) {
+       return response || fetch(e.request)
+     })
+   )
 })
