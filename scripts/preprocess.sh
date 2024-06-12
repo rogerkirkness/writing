caa860f889b2bbb65d17335aeab186fdc321cb9c
Roger Kirkness <me@rogerkirkness.com>
2024-06-12 16:52:07 -0400

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -15,8 +15,8 @@ find _posts -name '*.md' | sort | while IFS= read -r file; do
         title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
         # Use LaTeX to position the title and include the honeycomb design
         echo -e "\n\n\\vspace*{\\fill}\n# $title\n" >> "$output_file"
-        echo -e "\n\\begin{center}\n\\includegraphics[width=\\textwidth]{$honeycomb_image_path}\n\\end{center}\n" >> "$output_file"
-        echo -e "\\vspace*{\\fill}\n\\\\newpage\n\n" >> "$output_file"
+        # echo -e "\n\\begin{center}\n\\includegraphics[width=\\textwidth]{$honeycomb_image_path}\n\\end{center}\n" >> "$output_file"
+        echo -e "\\vspace*{\\fill}\n\\newpage\n\n" >> "$output_file"
         # Append the content of the file, skipping the YAML front matter
         sed -n '/^---$/,/^---$/!p' "$file" >> "$output_file"
     else
