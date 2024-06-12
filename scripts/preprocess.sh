9e8b81c15d6983259ebb4869c0a7b6f95cf6a180
Roger Kirkness <me@rogerkirkness.com>
2024-06-12 19:27:54 -0400

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -9,7 +9,10 @@ find _posts -name '*.md' | sort | while IFS= read -r file; do
         title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
         echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n# $title\n\n" >> "$output"
         sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"  # Strip YAML front matter
-        echo -e "\n\n\\\\newpage\n\n" >> "$output"
+       # Add a new page unless it's the last file
+        if [ $((i + 1)) -lt $total_files ]; then
+            echo -e "\n\n\\\\newpage\n\n" >> "$output"
+        fi
     else
         echo "Skipping missing file $file"
     fi
