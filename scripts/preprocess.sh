49746ad89a9183764c78bf281176b08fdc556978
Roger Kirkness <me@rogerkirkness.com>
2024-06-12 17:04:43 -0400

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+image_path="images/honeycomb.jpeg"
 output="pdf/combined.md"
 echo "" > "$output"
 
@@ -7,7 +8,10 @@ find _posts -name '*.md' | sort | while IFS= read -r file; do
     if [ -f "$file" ]; then
         echo "Processing $file"
         title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
-        echo -e "\n\n\\\\vspace*{0.33\\\\textheight}\n# $title\n\n" >> "$output"
+        echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n\\begin{center}\n# $title\n\n" >> "$output"
+
+        echo -e "\\\\vspace*{0.07\\\\textheight} % Additional space to position the image at about 40%\n\\\\begin{center}\n\\\\includegraphics[width=0.8\\\\textwidth]{$image_path}\n\\\\end{center}\n" >> "$output"
+        
         sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"  # Strip YAML front matter
         echo -e "\n\n\\\\newpage\n\n" >> "$output"
     else
