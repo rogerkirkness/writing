86d9260e3d2ec35548b934b119a39f28e7aa7ede
Roger Kirkness <me@rogerkirkness.com>
2024-06-12 19:40:32 -0400

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -19,14 +19,14 @@ done
 # Process each tag and its associated posts
 for tag in "${!tag_posts[@]}"; do
     # Create a title page for each tag
-    echo -e "\n\n\\vspace*{0.30\\textheight}\n\\begin{center}\n# ${tag^}\n\\end{center}\n" >> "$output"
-    echo -e "\n\n\\newpage\n\n" >> "$output"
+    echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n\\\\begin{center}\n# ${tag^}\n\\\\end{center}\n" >> "$output"
+    echo -e "\n\n\\\\newpage\n\n" >> "$output"
     # Sort and iterate over each file associated with this tag
     for file in ${tag_posts[$tag]}; do
         echo "Processing $file under tag $tag"
         title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
-        echo -e "\n\n\\vspace*{0.30\\textheight}\n# $title\n\n" >> "$output"
+        echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n# $title\n\n" >> "$output"
         sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"  # Strip YAML front matter
-        echo -e "\n\n\\newpage\n\n" >> "$output"
+        echo -e "\n\n\\\\newpage\n\n" >> "$output"
     done
 done
