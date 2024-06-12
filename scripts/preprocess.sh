b533bcdc7a41d2b55a52929fa74d8bfcc9f7a4a1
Roger Kirkness <me@rogerkirkness.com>
2024-06-12 16:58:04 -0400

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -7,7 +7,7 @@ find _posts -name '*.md' | sort | while IFS= read -r file; do
     if [ -f "$file" ]; then
         echo "Processing $file"
         title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
-        echo -e "\n\n\\vspace*{0.33\\textheight}\n# $title\n\n" >> "$output"
+        echo -e "\n\n\\\\vspace*{0.33\\\\textheight}\n# $title\n\n" >> "$output"
         sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"  # Strip YAML front matter
         echo -e "\n\n\\\\newpage\n\n" >> "$output"
     else
