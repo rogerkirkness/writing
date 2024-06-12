e477df6e6c4573b14664df186be9e46469dbb683
Roger Kirkness <me@rogerkirkness.com>
2024-06-12 16:42:37 -0400

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-output="pdf/combined.md"
-echo "" > "$output"
+output_dir="pdf"
+output_file="${output_dir}/combined.md"
+honeycomb_image_path="/images/honeycomb.webp"  # Update this path
+
+# Ensure the output directory exists
+mkdir -p "$output_dir"
+echo "" > "$output_file"
 
 find _posts -name '*.md' | sort | while IFS= read -r file; do
     if [ -f "$file" ]; then
         echo "Processing $file"
+        # Extract title ensuring to remove front matter properly
         title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
-        echo -e "\n\n# $title\n\n" >> "$output"
-        sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"  # Strip YAML front matter
-        echo -e "\n\n\\\\newpage\n\n" >> "$output"
+        # Use LaTeX to position the title and include the honeycomb design
+        echo -e "\n\n\\vspace*{\\fill}\n# $title\n" >> "$output_file"
+        echo -e "\n\\begin{center}\n\\includegraphics[width=\\textwidth]{$honeycomb_image_path}\n\\end{center}\n" >> "$output_file"
+        echo -e "\\vspace*{\\fill}\n\\\\newpage\n\n" >> "$output_file"
+        # Append the content of the file, skipping the YAML front matter
+        sed -n '/^---$/,/^---$/!p' "$file" >> "$output_file"
     else
         echo "Skipping missing file $file"
     fi
