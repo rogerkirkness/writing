d8b639e2b88dffe1c5d0abcde161203f5283c401
Roger Kirkness <me@rogerkirkness.com>
2024-06-12 20:03:20 -0400

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -5,12 +5,16 @@ echo "" > "$output"
 
 find _posts -name '*.md' | sort | while IFS= read -r file; do
     if [ -f "$file" ]; then
-        echo "Processing $file"
-        title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
-        echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n# $title\n\n" >> "$output"
-        sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"  # Strip YAML front matter
-        echo -e "\n\n\\\\newpage\n\n" >> "$output"
-    else
-        echo "Skipping missing file $file"
-    fi
+        # Check if 'book' is set to 'false' in the front matter
+        book_status=$(sed -n '/^book: false/p' "$file")
+        if [ -z "$book_status" ]; then  # Only proceed if 'book: false' is not found
+            echo "Processing $file"
+            title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
+            # Insert more code to handle tags if needed
+            echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n# $title\n\n" >> "$output"
+            sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"  # Strip YAML front matter
+            echo -e "\n\n\\\\newpage\n\n" >> "$output"
+        else
+            echo "Skipping $file (book set to false)"
+        fi
 done
