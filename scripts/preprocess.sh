c5021970723f66e50b0f6ffb4b1b108eb1c25b01
Roger Kirkness <roger.kirkness@convictional.com>
2024-06-12 21:30:39 -0400

    .

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -3,33 +3,46 @@
 output="pdf/combined.md"
 echo "" > "$output"
 
-# Holds tags
-declare -A tags
+# Arrays for tags and files
+declare -a tagsArray
+declare -a filesArray
+
+# Function to add or append files to arrays
+add_or_append() {
+    local newtag="$1"
+    local newfile="$2"
+    for i in "${!tagsArray[@]}"; do
+        if [[ "${tagsArray[i]}" == "$newtag" ]]; then
+            filesArray[i]+="$newfile "
+            return
+        fi
+    done
+    tagsArray+=("$newtag")
+    filesArray+=("$newfile ")
+}
 
 # Collect posts and their tags
 find _posts -name '*.md' | while IFS= read -r file; do
     if [ -f "$file" ]; then
         book_status=$(sed -n '/^book: false/p' "$file")
-        if [ -z "$book_status" ]; then  # Proceed only if 'book: false' is not found
+        if [ -z "$book_status" ]; then  # Only proceed if 'book: false' is not found
             tag=$(sed -n -e 's/^tags: \[\(.*\)\]/\1/p' "$file")
-            tags[$tag]+="$file "  # Append file to the list of files for this tag
-            echo "The post $file is in the $tag category"
+            tag=${tag^^}  # Convert tag to uppercase
+            add_or_append "$tag" "$file"
         fi
     fi
 done
 
-echo "Tags found: ${!tags[@]}"
-
-# Process each tag and its associated files
-for tag in "${!tags[@]}"; do
+# Process each tag and associated files
+for i in "${!tagsArray[@]}"; do
+    tag="${tagsArray[i]}"
     echo "Processing tag: $tag"
-    echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n\\begin{center}\n# ${tag^}\n\\end{center}\n" >> "$output"
-    for file in ${tags[$tag]}; do
-        echo "Processing file: $file"
+    echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n\\begin{center}\n# $tag\n\\end{center}\n" >> "$output"
+    files="${filesArray[i]}"
+    for file in $files; do
         title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
         echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n# $title\n\n" >> "$output"
         sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"  # Strip YAML front matter
         echo -e "\n\n\\\\newpage\n\n" >> "$output"
     done
-    echo -e "\n\n\\\\newpage\n\n" >> "$output"  # Add a page break after each tag section
 done
