c17cddac41381deaf7894d0c1fda0ac5b5d71786
Roger Kirkness <roger@convictional.com>
2024-06-13 14:07:19 -0400

    .

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -39,13 +39,13 @@ done < <(find _posts -name '*.md' | sort)
 # Process each tag and associated files
 for i in "${!tagsArray[@]}"; do
     tag="${tagsArray[i]}"
+    echo -e "\n\\\\addcontentsline{toc}{section}{$tag}\n" >> "$output"
     echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n\\\\begin{center}\n\\\\Huge $tag\n\\\\end{center}\n" >> "$output"
     echo -e "\n\\\\begin{center}\n\\\\includegraphics[width=0.2\\\\textwidth]{$honeycombs_image}\n\\\\end{center}\n" >> "$output"
     echo -e "\n\n\\\\newpage\n\n" >> "$output"
     files="${filesArray[i]}"
     for file in $files; do
         title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
-        echo -e "\n\\\\addcontentsline{toc}{subsection}{$title}\n" >> "$output"
         echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n\\\\begin{center}\n\\\\Large $title\n\\\\end{center}\n" >> "$output"
         echo -e "\n\\\\begin{center}\n\\\\includegraphics[width=0.1\\\\textwidth]{$honeycomb_image}\n\\\\end{center}\n" >> "$output"
         sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"
