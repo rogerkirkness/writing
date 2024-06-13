8949e293f34ba35439d3c6465d7edf91d25bf280
Roger Kirkness <roger@convictional.com>
2024-06-13 09:51:57 -0400

    Fixed the script finally, issue was a race condition

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -14,7 +14,6 @@ add_or_append() {
     for i in "${!tagsArray[@]}"; do
         if [[ "${tagsArray[i]}" == "$newtag" ]]; then
             filesArray[i]+="$newfile "
-            echo filesArray[i]
             return
         fi
     done
@@ -22,28 +21,27 @@ add_or_append() {
     filesArray+=("$newfile ")
 }
 
-# Collect posts and their tags
-find _posts -name '*.md' | while IFS= read -r file; do
+# Avoid using a subshell for the loop
+while IFS= read -r file; do
     if [ -f "$file" ]; then
         book_status=$(sed -n '/^book: false/p' "$file")
         if [ -z "$book_status" ]; then  # Only proceed if 'book: false' is not found
             tag=$(sed -n -e 's/^tags: \[\(.*\)\]/\1/p' "$file")
-            tag=${tag^^}  # Convert tag to uppercase
             add_or_append "$tag" "$file"
         fi
     fi
-done
+done < <(find _posts -name '*.md' | sort)
 
 # Process each tag and associated files
 for i in "${!tagsArray[@]}"; do
     tag="${tagsArray[i]}"
     echo "Processing tag: $tag"
-    echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n\\begin{center}\n# $tag\n\\end{center}\n" >> "$output"
+    echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n\\\\begin{center}\n# $tag\n\\end{center}\n" >> "$output"
     files="${filesArray[i]}"
     for file in $files; do
         title=$(sed -n '/^title: /p' "$file" | sed 's/title: //')
         echo -e "\n\n\\\\vspace*{0.30\\\\textheight}\n# $title\n\n" >> "$output"
-        sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"  # Strip YAML front matter
+        sed -e '1,/^\---$/d' -e '/^\---$/,$d' "$file" >> "$output"
         echo -e "\n\n\\\\newpage\n\n" >> "$output"
     done
 done
